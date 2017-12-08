86d2237aad118941cc2af6d7db3474e5a4f2f0db
yajamon <yajamon.tatsuki@gmail.com>
2017-12-08 19:16:04 +0900

    Use HttpsConnector

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 extern crate futures;
 extern crate hyper;
+extern crate hyper_tls;
 extern crate tokio_core;
 
 use std::io::{self, Write};
 use futures::{Future, Stream};
 use hyper::Client;
+use hyper_tls::HttpsConnector;
 use tokio_core::reactor::Core;
 
 fn main() {
     let mut core = Core::new().unwrap();
-    let client = Client::new(&core.handle());
+    let handle = core.handle();
+    let client = Client::configure()
+        .connector(HttpsConnector::new(4, &handle).unwrap())
+        .build(&handle);
 
-    //let uri = match "https://api.zaif.jp/api/1/currencies/btc".parse() {
-    let uri = "http://www.example.com".parse().unwrap();
+    let uri = "https://api.zaif.jp/api/1/currencies/btc".parse().unwrap();
 
     let work = client.get(uri).and_then(|res| {
         println!("Response: {}", res.status());
