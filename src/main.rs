09817195424838753b39adf930048fcb68ac697c
yajamon <yajamon.tatsuki@gmail.com>
2017-12-08 18:58:34 +0900

    Simple http request

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,30 @@ use hyper::Client;
 use tokio_core::reactor::Core;
 
 fn main() {
+    let mut core = match Core::new() {
+        Ok(v) => v,
+        Err(_) => panic!("error"),
+    };
+    let client = Client::new(&core.handle());
 
+    //let uri = match "https://api.zaif.jp/api/1/currencies/btc".parse() {
+    let uri = match "http://www.example.com/".parse() {
+        Ok(v) => v,
+        Err(_) => panic!("error"),
+    };
+
+    let work = client.get(uri).and_then(|res| {
+        println!("Response: {}", res.status());
+
+        res.body().for_each(|chunk| {
+            io::stdout()
+                .write_all(&chunk)
+                .map(|_| ())
+                .map_err(From::from)
+        })
+    });
+    match core.run(work) {
+        Ok(_) => println!("ok"),
+        Err(_) => panic!("error"),
+    }
 }
