933969e20b1667bc5067d7cc2ae390559e994c67
yajamon <yajamon.tatsuki@gmail.com>
2018-01-01 17:20:16 +0900

    Apiにuriフィールドを追加

--- src/core/api.rs
+++ src/core/api.rs
@@ -1,24 +1,32 @@
 extern crate reqwest;
 
-pub struct Api {}
+pub struct Api {
+    uri: String,
+}
 
 impl Api {
     fn exec(&self) -> reqwest::Result<String> {
-        let mut resp = reqwest::get("https://example.com")?;
+        let mut resp = reqwest::get(self.uri.as_str())?;
 
         assert!(resp.status().is_success());
         resp.text()
     }
 }
 
-pub struct ApiBuilder {}
+pub struct ApiBuilder {
+    uri: String,
+}
 
 impl ApiBuilder {
     fn new() -> ApiBuilder {
-        ApiBuilder {}
+        ApiBuilder {
+            uri: "".to_string(),
+        }
     }
 
     fn finalize(&self) -> Api {
-        Api {}
+        Api {
+            uri: self.uri.clone(),
+        }
     }
 }
