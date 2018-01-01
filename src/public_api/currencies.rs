d55d49159c837ccda1eca1b2b02e6f58e97a6718
yajamon <yajamon.tatsuki@gmail.com>
2018-01-01 19:22:29 +0900

    Currenciesのname要素をfieldに変更

--- src/public_api/currencies.rs
+++ src/public_api/currencies.rs
@@ -2,18 +2,37 @@ extern crate reqwest;
 
 use core::*;
 
-pub struct Currencies {}
+pub struct Currencies {
+    name: String,
+}
 
 impl Currencies {
-    pub fn get(&self, name: &str) -> reqwest::Result<String> {
-        self.exec(name)
-    }
-
-    pub fn exec(&self, name: &str) -> reqwest::Result<String> {
+    pub fn exec(&self) -> reqwest::Result<String> {
         let api = ApiBuilder::new()
-            .uri(format!("https://api.zaif.jp/api/1/currencies/{}", name).as_str())
+            .uri(format!("https://api.zaif.jp/api/1/currencies/{}", self.name).as_str())
             .finalize();
 
         api.exec()
+    }
+}
+
+pub struct CurrenciesBuilder {
+    name: String,
+}
+
+impl CurrenciesBuilder {
+    pub fn new() -> CurrenciesBuilder {
+        CurrenciesBuilder {
+            name: "all".to_string(),
+        }
+    }
+    pub fn name(&mut self, name: &str) -> &mut CurrenciesBuilder {
+        self.name = name.to_string();
+        self
+    }
+    pub fn finalize(&self) -> Currencies {
+        Currencies {
+            name: self.name.clone(),
+        }
     }
 }
