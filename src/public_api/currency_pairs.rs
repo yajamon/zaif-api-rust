d94e40e209103b40588ccfb8ba311f45816f254e
yajamon <yajamon.tatsuki@gmail.com>
2018-01-01 22:02:28 +0900

    core::Apiを使ってcurrency_pairsを再実装

--- src/public_api/currency_pairs.rs
+++ src/public_api/currency_pairs.rs
@@ -1,13 +1,15 @@
 extern crate reqwest;
 
+use core::*;
+
 pub struct CurrencyPairs;
 
 impl CurrencyPairs {
     pub fn get(&self, name: &str) -> reqwest::Result<String> {
-        let uri = format!("https://api.zaif.jp/api/1/currency_pairs/{}", name);
-        let mut resp = reqwest::get(uri.as_str())?;
+        let api = ApiBuilder::new()
+            .uri(format!("https://api.zaif.jp/api/1/currency_pairs/{}", name).as_str())
+            .finalize();
 
-        assert!(resp.status().is_success());
-        resp.text()
+        api.exec()
     }
 }
