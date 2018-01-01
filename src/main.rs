3381f4501dae65cc08e53df29dbf321fe2556134
yajamon <yajamon.tatsuki@gmail.com>
2018-01-01 16:00:22 +0900

    Add api: CurrencyPairs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 extern crate zaif_api;
 
 use zaif_api::public_api::currencies::Currencies;
+use zaif_api::public_api::currency_pairs::CurrencyPairs;
 use zaif_api::trade_api::get_info2::GetInfo2;
 
 fn main() {
     // let api = Currencies {};
     // println!("{}", api.get("btc").unwrap());
 
-    let api = GetInfo2::new("YOUR_API_KEY", "YOUR_API_SECRET");
-    api.post();
+    let api = CurrencyPairs {};
+    println!("{}", api.get("all").unwrap());
+
+    // let api = GetInfo2::new("YOUR_API_KEY", "YOUR_API_SECRET");
+    // api.post();
 }
--- src/public_api/currency_pairs.rs
+++ src/public_api/currency_pairs.rs
@@ -1,3 +1,13 @@
+extern crate reqwest;
+
 pub struct CurrencyPairs;
 
-impl CurrencyPairs {}
+impl CurrencyPairs {
+    pub fn get(&self, name: &str) -> reqwest::Result<String> {
+        let uri = format!("https://api.zaif.jp/api/1/currency_pairs/{}", name);
+        let mut resp = reqwest::get(uri.as_str())?;
+
+        assert!(resp.status().is_success());
+        resp.text()
+    }
+}
