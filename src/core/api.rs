267c64b7b90d6b5cc857ec2bdd179bdea64ca5cd
yajamon <yajamon.tatsuki@gmail.com>
2018-01-01 17:03:26 +0900

    Api.exec()を仮実装

--- src/core/api.rs
+++ src/core/api.rs
@@ -3,13 +3,20 @@ extern crate reqwest;
 pub struct Api {}
 
 impl Api {
-    fn exec(&self) {}
+    fn exec(&self) -> reqwest::Result<String> {
+        let mut resp = reqwest::get("https://example.com")?;
+
+        assert!(resp.status().is_success());
+        resp.text()
+    }
 }
 
 pub struct ApiBuilder {}
 
 impl ApiBuilder {
-    fn new() -> ApiBuilder {}
+    fn new() -> ApiBuilder {
+        ApiBuilder {}
+    }
 
     fn finalize(&self) -> Api {
         Api {}
