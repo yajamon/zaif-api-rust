f2e01ed160f15c8473e8b5d042ab79cc9f730ac5
yajamon <yajamon.tatsuki@gmail.com>
2018-01-07 17:03:42 +0900

    GetInfo2をcore/Apiを用いた実装に変更

--- src/trade_api/get_info2.rs
+++ src/trade_api/get_info2.rs
@@ -1,63 +1,30 @@
 extern crate reqwest;
-extern crate chrono;
-extern crate openssl;
 
-use std::io::{self, Write};
-use self::reqwest::header::Headers;
-use self::chrono::Utc;
-use self::openssl::hash::MessageDigest;
-use self::openssl::pkey::PKey;
-use self::openssl::sign::Signer;
+use std::collections::HashMap;
+
+use core::*;
 
 pub struct GetInfo2 {
-    api_key: String,
-    api_secret: String,
+    access_key: AccessKey,
 }
 
 impl GetInfo2 {
-    pub fn new(key: &str, secret: &str) -> GetInfo2 {
+    pub fn new(access_key: AccessKey) -> GetInfo2 {
         GetInfo2 {
-            api_key: key.to_string(),
-            api_secret: secret.to_string(),
+            access_key: access_key,
         }
     }
-    pub fn post(&self) {
+    pub fn exec(&self) -> reqwest::Result<String> {
+        let param: &mut HashMap<String, String> = &mut HashMap::new();
+        param.insert("method".to_string(), "get_info2".to_string());
 
+        let api = ApiBuilder::new()
+            .access_key(self.access_key.clone())
+            .uri("https://api.zaif.jp/tapi")
+            .method(Method::Post)
+            .param(param.clone())
+            .finalize();
 
-        // body生成
-        let now = Utc::now();
-        let nonce = format!("{}.{}", now.timestamp(), now.timestamp_subsec_millis());
-        let param = [
-            ["method", "get_info2"].join("="),
-            ["nonce", nonce.as_str()].join("="),
-        ];
-        let body = param.join("&");
-
-        let key = PKey::hmac(self.api_secret.as_bytes()).unwrap();
-        let mut signer = Signer::new(MessageDigest::sha512(), &key).unwrap();
-        signer.update(body.as_bytes());
-        // Rust: byte array to hex String
-        // http://illegalargumentexception.blogspot.jp/2015/05/rust-byte-array-to-hex-string.html
-        let signed = signer.sign_to_vec().unwrap();
-        let strs:Vec<String> = signed.iter().map(|b| format!("{:02x}", b)).collect();
-        let sign = strs.join("");
-        // println!("Key: {}", self.api_key.as_str());
-        // println!("Body: {}", body);
-        // println!("Sign: {}", sign);
-
-        let mut headers = Headers::new();
-
-        headers.set_raw("Key", self.api_key.as_str());
-        headers.set_raw("Sign", sign.as_str());
-
-        let client = reqwest::Client::new();
-        let uri = "https://api.zaif.jp/tapi";
-        let mut res = client.post(uri)
-            .headers(headers)
-            .body(body)
-            .send().unwrap();
-        let res_body = res.text().unwrap();
-
-        io::stdout().write(res_body.as_bytes()).unwrap();
+        api.exec()
     }
 }
