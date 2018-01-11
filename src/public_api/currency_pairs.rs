022ae574588ecb55df4ba6d07a55caf4ef6da4be
yajamon <yajamon.tatsuki@gmail.com>
2018-01-11 14:57:09 +0900

    CurrencyPairs.nameをcurrency_pairにリネーム

--- src/public_api/currency_pairs.rs
+++ src/public_api/currency_pairs.rs
@@ -3,13 +3,13 @@ extern crate reqwest;
 use core::*;
 
 pub struct CurrencyPairs {
-    name: String,
+    currency_pair: String,
 }
 
 impl CurrencyPairs {
     pub fn exec(&self) -> reqwest::Result<String> {
         let api = ApiBuilder::new()
-            .uri(format!("https://api.zaif.jp/api/1/currency_pairs/{}", self.name).as_str())
+            .uri(format!("https://api.zaif.jp/api/1/currency_pairs/{}", self.currency_pair).as_str())
             .finalize();
 
         api.exec()
@@ -17,22 +17,22 @@ impl CurrencyPairs {
 }
 
 pub struct CurrencyPairsBuilder {
-    name: String,
+    currency_pair: String,
 }
 
 impl CurrencyPairsBuilder {
     pub fn new() -> CurrencyPairsBuilder {
         CurrencyPairsBuilder {
-            name: "all".to_string(),
+            currency_pair: "all".to_string(),
         }
     }
-    pub fn name(&mut self, name: &str) -> &mut CurrencyPairsBuilder {
-        self.name = name.to_string();
+    pub fn currency_pair(&mut self, currency_pair: &str) -> &mut CurrencyPairsBuilder {
+        self.currency_pair = currency_pair.to_string();
         self
     }
     pub fn finalize(&self) -> CurrencyPairs {
         CurrencyPairs {
-            name: self.name.clone(),
+            currency_pair: self.currency_pair.clone(),
         }
     }
 }
