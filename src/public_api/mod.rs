b8c005841b4a567859415e807347adb341890054
yajamon <yajamon.tatsuki@gmail.com>
2018-01-18 18:40:42 +0900

    PublicApi Traitを書いてみた

--- src/public_api/mod.rs
+++ src/public_api/mod.rs
@@ -7,3 +7,18 @@ mod currencies;
 mod currency_pairs;
 mod last_price;
 mod depth;
+
+trait PublicApi {
+    fn action(&self) -> &str;
+    fn parameter(&self) -> &str;
+    fn exec(&self) -> reqwest::Result<String> {
+        let endpoint = "https://api.zaif.jp/api/1";
+        let api = ApiBuilder::new()
+            .uri(
+                format!("{}/{}/{}", endpoint, self.action(), self.parameter()).as_str(),
+            )
+            .finalize();
+
+        api.exec()
+    }
+}
