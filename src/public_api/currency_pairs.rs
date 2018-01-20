e1a57e98e0a3408f612b201a4b5663cd5288a555
yajamon <yajamon.tatsuki@gmail.com>
2018-01-20 22:02:26 +0900

    CurrencyPairsBuilderをマクロで実装した

--- src/public_api/currency_pairs.rs
+++ src/public_api/currency_pairs.rs
@@ -2,9 +2,9 @@ extern crate reqwest;
 
 use core::*;
 
-pub struct CurrencyPairs {
-    currency_pair: String,
-}
+builder!(CurrencyPairsBuilder => CurrencyPairs {
+    currency_pair: String = "all".to_string()
+});
 
 impl CurrencyPairs {
     pub fn exec(&self) -> reqwest::Result<String> {
@@ -21,19 +21,3 @@ impl CurrencyPairs {
     }
 }
 
-pub struct CurrencyPairsBuilder {
-    currency_pair: String,
-}
-
-impl CurrencyPairsBuilder {
-    pub fn new() -> CurrencyPairsBuilder {
-        CurrencyPairsBuilder { currency_pair: "all".to_string() }
-    }
-    pub fn currency_pair(&mut self, currency_pair: &str) -> &mut CurrencyPairsBuilder {
-        self.currency_pair = currency_pair.to_string();
-        self
-    }
-    pub fn finalize(&self) -> CurrencyPairs {
-        CurrencyPairs { currency_pair: self.currency_pair.clone() }
-    }
-}
