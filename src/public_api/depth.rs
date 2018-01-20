0d71f39eec753f6fa9e166c7c47829e1ad658955
yajamon <yajamon.tatsuki@gmail.com>
2018-01-20 22:07:12 +0900

    DepthBuilderをマクロで実装した

--- src/public_api/depth.rs
+++ src/public_api/depth.rs
@@ -2,9 +2,9 @@ extern crate reqwest;
 
 use core::*;
 
-pub struct Depth {
-    currency_pair: String,
-}
+builder!(DepthBuilder => Depth {
+    currency_pair: String = "btc_jpy".to_string()
+});
 
 impl Depth {
     pub fn exec(&self) -> reqwest::Result<String> {
@@ -18,19 +18,3 @@ impl Depth {
     }
 }
 
-pub struct DepthBuilder {
-    currency_pair: String,
-}
-
-impl DepthBuilder {
-    pub fn new() -> DepthBuilder {
-        DepthBuilder { currency_pair: "btc_jpy".to_string() }
-    }
-    pub fn currency_pair(&mut self, currency_pair: &str) -> &mut DepthBuilder {
-        self.currency_pair = currency_pair.to_string();
-        self
-    }
-    pub fn finalize(&self) -> Depth {
-        Depth { currency_pair: self.currency_pair.clone() }
-    }
-}
