92a5d44181e2797a0e272e2d310263a4fda80f8b
yajamon <yajamon.tatsuki@gmail.com>
2018-02-09 20:57:05 +0900

    TradeAPIのお試し処理を個別処理に切り出し

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,15 @@ use zaif_api::trade_api::*;
 fn put_help() {
     let s = "
 command [action]
+        currency
+        currency_pair
+        last_price
+        depth
+        trades
+        ticker
+        get_info2
+        trade
+        active_orders
 ";
     println!("{}", s);
 }
@@ -25,6 +34,8 @@ fn main() {
     let action = &args[1];
     println!("action: {}", action);
 
+    let access_key = AccessKey::new("YOUR_ACCESS_KEY", "YOUR_SECRET");
+
     match action.as_str() {
         "currency" => call_currency(),
         "currency_pair" => call_currency_pair(),
@@ -32,71 +43,12 @@ fn main() {
         "depth" => call_depth(),
         "trades" => call_trades(),
         "ticker" => call_ticker(),
+        "get_info2" => call_get_info2(&access_key),
+        "trade" => call_trade(&access_key),
+        "active_orders" => call_active_orders(&access_key),
         _ => put_help(),
     }
     return;
-
-    let access_key = AccessKey::new("YOUR_API_KEY", "YOUR_API_SECRET");
-    let api = GetInfo2Builder::new()
-        .access_key(access_key.clone())
-        .finalize();
-    for (coin, amount) in api.exec().unwrap().funds.iter() {
-        println!("coin: {} amount: {}", coin, amount);
-    }
-
-    let api = TradeBuilder::new()
-        .access_key(access_key.clone())
-        .currency_pair("zaif_jpy".to_string())
-        .action(TradeAction::Bid)
-        .price(1.0)
-        .amount(0.1)
-        .finalize();
-    match api.exec()
-        .and_then(|res| {
-            println!(
-                "received: {}, remains: {}, order_id: {}",
-                res.received,
-                res.remains,
-                res.order_id
-            );
-            if res.order_id == 0 {
-                panic!("Complete trade.");
-            }
-            Ok(res.order_id)
-        })
-        .and_then(|order_id| {
-            let api = CancelOrderBuilder::new()
-                .access_key(access_key.clone())
-                .order_id(order_id)
-                .currency_pair(Some("zaif_jpy".to_string()))
-                .finalize();
-            let wait_time = time::Duration::from_secs(5);
-            thread::sleep(wait_time);
-            api.exec()
-        })
-        .and_then(|res| {
-            println!("Cancel order_id: {}", res.order_id);
-            Ok(())
-        }) {
-
-        Ok(_) => println!("Complete trade and cancel"),
-        Err(e) => println!("Error: {}", e),
-    }
-
-    let api = ActiveOrdersBuilder::new()
-        .access_key(access_key.clone())
-        .currency_pair(Some("zaif_jpy".to_string()))
-        .finalize();
-    for (order_id, order) in api.exec().unwrap().iter() {
-        println!(
-            "order_id: {}, currency_pair: {}, action: {}, amount: {}, price: {}",
-            order_id,
-            order.currency_pair,
-            order.action,
-            order.amount,
-            order.price
-        );
-    }
 }
 
 fn call_currency() {
@@ -158,4 +110,72 @@ fn call_ticker() {
         .finalize();
     let res = api.exec().unwrap();
     println!("last: {}, high: {}, low: {}", res.last, res.high, res.low);
+}
+
+fn call_get_info2(access_key: &AccessKey) {
+    let api = GetInfo2Builder::new()
+        .access_key(access_key.clone())
+        .finalize();
+    for (coin, amount) in api.exec().unwrap().funds.iter() {
+        println!("coin: {} amount: {}", coin, amount);
+    }
+}
+
+fn call_trade(access_key: &AccessKey) {
+    let api = TradeBuilder::new()
+        .access_key(access_key.clone())
+        .currency_pair("zaif_jpy".to_string())
+        .action(TradeAction::Bid)
+        .price(1.0)
+        .amount(0.1)
+        .finalize();
+    match api.exec()
+        .and_then(|res| {
+            println!(
+                "received: {}, remains: {}, order_id: {}",
+                res.received,
+                res.remains,
+                res.order_id
+            );
+            if res.order_id == 0 {
+                panic!("Complete trade.");
+            }
+            Ok(res.order_id)
+        })
+        .and_then(|order_id| {
+            let api = CancelOrderBuilder::new()
+                .access_key(access_key.clone())
+                .order_id(order_id)
+                .currency_pair(Some("zaif_jpy".to_string()))
+                .finalize();
+            let wait_time = time::Duration::from_secs(5);
+            thread::sleep(wait_time);
+            api.exec()
+        })
+        .and_then(|res| {
+            println!("Cancel order_id: {}", res.order_id);
+            Ok(())
+        }) {
+
+        Ok(_) => println!("Complete trade and cancel"),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+fn call_active_orders(access_key: &AccessKey) {
+
+    let api = ActiveOrdersBuilder::new()
+        .access_key(access_key.clone())
+        .currency_pair(Some("zaif_jpy".to_string()))
+        .finalize();
+    for (order_id, order) in api.exec().unwrap().iter() {
+        println!(
+            "order_id: {}, currency_pair: {}, action: {}, amount: {}, price: {}",
+            order_id,
+            order.currency_pair,
+            order.action,
+            order.amount,
+            order.price
+        );
+    }
 }
