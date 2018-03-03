6b3c15438de9b80118cd3f5f51e77f9a7b8693c1
yajamon <yajamon.tatsuki@gmail.com>
2018-03-03 18:51:36 +0900

    serde_json::Error, reqwest::Errorを取り込めるようにした

--- src/error.rs
+++ src/error.rs
@@ -10,3 +10,14 @@ pub enum Error {
 }
 
 pub type Result<T> = ::std::result::Result<T, Error>;
+
+impl From<reqwest::Error> for Error {
+    fn from(error: reqwest::Error) -> Self {
+        Error::ReqwestError(error)
+    }
+}
+impl From<serde_json::Error> for Error {
+    fn from(error: serde_json::Error) -> Self {
+        Error::SerdeJsonError(error)
+    }
+}
